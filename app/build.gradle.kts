12041c364819f4885a67ac66872452a0ed3cf460
KSnie <u6510348@au.edu>
2025-03-01 00:48:23 +0700

    CommitsNine

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,25 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.runtime.livedata)
+    val room_version = "2.6.1"
+
+    implementation(libs.androidx.room.runtime)
+    // If this project only uses Java source, use the Java annotationProcessor
+    // No additional plugins are necessary
+    annotationProcessor(libs.androidx.room.compiler)
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation(libs.androidx.room.ktx)
+    // optional - RxJava2 support for Room
+    implementation(libs.androidx.room.rxjava2)
+    // optional - RxJava3 support for Room
+    implementation(libs.androidx.room.rxjava3)
+    // optional - Guava support for Room, including Optional and ListenableFuture
+    implementation(libs.androidx.room.guava)
+    // optional - Test helpers
+    testImplementation(libs.androidx.room.testing)
+    // optional - Paging 3 Integration
+    implementation(libs.androidx.room.paging)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -59,6 +78,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
 
     implementation(libs.firebase.bom)
     implementation(libs.firebase.analytics)
