4cd9dda77b59ea38fe9d52894a58ef7665f66a9e
KSnie <u6510348@au.edu>
2025-02-28 23:42:40 +0700

    CommitsNine

--- app/src/main/java/com/example/projectapp/MainActivity.kt
+++ app/src/main/java/com/example/projectapp/MainActivity.kt
@@ -108,7 +108,7 @@ fun HomeScreen(onNavigateToIncome: () -> Unit, onThemeToggle: () -> Unit) {
         ) {
             Column {
                         Text(text = "wwww", fontSize = 35.sp)
-            Text(text = "KasidisSSS", fontSize = 45.sp)
+            Text(text = "Kasidis", fontSize = 45.sp)
         }
 
         Row(
